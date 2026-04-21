24ce1c20e45026b8491fc283fe1491792373c7d1
Orynbek-ov <bekalyorynbekov40@gmail.com>
2026-04-21 14:58:35 +0500

    mk

--- Assets/scripty.fan4ik/AvatarData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Avatar", menuName = "Avatar System/Avatar Data")]
-public class AvatarData : ScriptableObject
-{
-    public string avatarName;
-    public string prefabResourceName; // Имя префаба в папке Resources
-    public Sprite avatarIcon;        // Иконка для UI
-}
